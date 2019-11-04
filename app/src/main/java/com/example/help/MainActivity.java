320cfb5a0a54770b12cc9c046097cb382ee021d0
iamcodeking <miscritslakshay58@gmail.com>
2019-11-03 16:19:07 -0800

    add

---
+++ app/src/main/java/com/example/help/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.help;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    EditText user,pass;
+    Button login;
+    TextView sign;
+    String user1;
+    String pass1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        user=findViewById(R.id.user);
+        pass=findViewById(R.id.password);
+        login=findViewById(R.id.login);
+        sign=findViewById(R.id.signup);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 user1=user.getText().toString();
+                 pass1=pass.getText().toString();
+                String url = "";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.equals("yes")) {
+                            Toast.makeText(MainActivity.this, "logged IN", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "incorrect username/password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> map=new HashMap<>();
+                        map.put("username",user1);
+                        map.put("password",pass1);
+                        return map;
+                    }
+                };
+                RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
+                requestQueue.add(stringRequest);
+                }
+
+            });
+    }
+}
